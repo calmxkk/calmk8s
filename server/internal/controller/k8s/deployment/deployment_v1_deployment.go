7f864193e6ef49d7ba071285ea311ab931f9eae8
calmxkk <zhaokai417@163.com>
2024-04-23 23:30:49 +0800

    add deployment api

--- server/internal/controller/k8s/deployment/deployment_v1_deployment.go
+++ server/internal/controller/k8s/deployment/deployment_v1_deployment.go
@@ -2,8 +2,10 @@ package deployment
 
 import (
 	"context"
+	"fmt"
 	"github.com/gogf/gf/v2/errors/gcode"
 	"github.com/gogf/gf/v2/errors/gerror"
+	"github.com/gogf/gf/v2/frame/g"
 
 	"calmk8s/api/k8s/deployment/v1"
 )
@@ -12,5 +14,7 @@ func (c *ControllerV1) List(ctx context.Context, req *v1.ListReq) (res *v1.ListR
 	return nil, gerror.NewCode(gcode.CodeNotImplemented)
 }
 func (c *ControllerV1) Get(ctx context.Context, req *v1.GetReq) (res *v1.GetRes, err error) {
-	return nil, gerror.NewCode(gcode.CodeNotImplemented)
+	r := g.RequestFromCtx(ctx)
+	fmt.Printf("namespace: %v\n", r.Get("namespace"))
+	return nil, err
 }
